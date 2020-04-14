09440eb5cd86d3ceba10965d49156f45f2c84c5a
MS Tulpakov <max-wp@mail.ru>
2020-04-14 22:15:07 +0300

    Предварительная разметка страницы

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Химмед - поставим реактивы, лабораторное оборудование, аналитическое оборудование.</title>
+    <meta name='description' content='Товары со склада в Москве'>
+    <meta name='keywords' content='Товары со склада в Москве'>
+</head>
+
+<body>
+    <header class="header-top">
+        <a href="mailto:vlad@htmlbook.ru">mail@chimmed.ru</a>
+        <nav class="site-navigation">
+            <ul>
+                <li>Доставка</li>
+                <li>Новости</li>
+                <li>Услуги</li>
+                <li>Преимущества</li>
+                <li>О нас</li>
+                <li>Контакты</li>
+            </ul>
+        </nav>
+        <span>9:00 - 17:30</span>
+        <span>Eng</span>
+        <a href="tel:+74956404192"> +7 (495) 640-41-92 </a>
+    </header>
+    <div class="header-search">
+        <a class="logo" href="#"><img src="" alt=""></a>
+        <ul class="catalog">
+            <li>Каталог - 01</li>
+            <li>Каталог - 02</li>
+            <li>Каталог - 03</li>
+            <li>Каталог - 04</li>
+            <li>Каталог - 05</li>
+            <li>Каталог - 06</li>
+            <li>Каталог - 07</li>
+            <li>Каталог - 08</li>
+            <li>Каталог - 09</li>
+            <li>Каталог - 10</li>
+            <li>Каталог - 11</li>
+            <li>Каталог - 12</li>
+        </ul>
+        <div class="search">
+            <div class="user-menu">
+                <span class="login"></span>
+                <span class="favourites"></span>
+                <span class="backet"></sopan>
+            </div>
+            <a href="" class="button-callback">Заказать звонок</a>
+        </div>
+
+    </div>
+    <div class="secondary-menu">
+        <ul>
+            <li>Акции</li>
+            <li>Поставщики</li>
+            <li>Товары в наличии</li>
+            <li>Производство</li>
+            <li>Реактивы & Стандарты</li>
+            <li>Life Sciences</li>
+            <li>Лабораторное оборудование</li>
+            <li>Аналитическое оборудование</li>
+            <li>Фармпроизводителям</li>
+            <li>Микроэлектроника</li>
+        </ul>
+    </div>
+    <div class="banner"><img src="" alt=""></div>
+    <section class="special-offer">
+        <h2>Специальные предложения</h2>
+        <ul class="catalog-special-list">
+            <li class="special-item">
+                <img src="" alt="" class="special-item-img">
+                <p class="special-item-description">Marketplace 1-2020: сэкономьте на продуктах, необходимых для ПЦР и
+                    электрофореза!</p>
+                <a href="" class="special-item-link">Подробнее</a>
+            </li>
+        </ul>
+
+</section>
+
+    <section class="catalog">
+        <h2>Наши товры</h2>
+        <ul class="catalog-list">
+            <li class="catalog-item">
+                <img src="" alt="" class="catalog-item-img">
+                <p class="catalog-item-price">2040 руб.</p>
+                <p class="catalog-item-description">Ацетонитрил ОСЧ для
+                    градиентной ВЭЖХ</p>
+                <a href="" class="catalog-item-link">Заказать</a>
+            </li>
+        </ul>
+</section>
+
+    <section class="news">
+        <div class="news-header">
+            <h2>Новости</h2>
+            <p class="news-link">Все новости</p>
+        </div>
+
+        <ul class="news-list">
+            <li class="news-item">
+                <img src="" alt="" class="news-item-img">
+                <h2>USP орум 15 апреля 2020</h2>
+                <span class="news-item-data">2020-02-20</span>
+                <p class="news-item-description">Ежегодный семинар по стандартам USP</p>
+
+
+            </li>
+
+        </ul>
+    </section>
+
+    <section class="calendar">
+
+    </section>
+    <section class="feedback">
+
+    </section>
+    <footer>
+        <div class="moscow"></div>
+        <div class="russia"></div>
+        <div class="footer-menu"></div>
+
+    </footer>
+
+</body>
+    .
+</html>
